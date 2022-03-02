39137c91644bb256e9ea7c28e336286a5698aa19
Sun <skyung0717@naver.com>
2022-03-02 23:54:50 +0900

    commit

--- homework/urls.py
+++ homework/urls.py
@@ -4,6 +4,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='my_main.html'), name='main'),
+    path('main/', TemplateView.as_view(template_name='my_main.html'), name='main'),
     path('admin/', admin.site.urls),
     path('login/', TemplateView.as_view(template_name='log_in.html'), name='login'),
     path('sun/', include('sun.urls'))
